d20840644f51452865e384e7c1eeb5c0032aa394
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-13 22:47:58 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './style/App.css';
+import './style/index.css';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+  } 
+
+  render() { 
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList />
+        </div>
+      </div>
+    );
+  }
+}
