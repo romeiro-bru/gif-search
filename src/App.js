c88c8d2431c52f705bceb897f530b770f4838dbb
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-08-14 19:22:10 -0300

    Initialize loading state

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     }
   } 
 
@@ -23,7 +24,8 @@ export default class App extends Component {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
     .then(response => {
       this.setState({
-        gifs: response.data.data
+        gifs: response.data.data,
+        loading: false
       });
     })
     .catch(error => {
@@ -42,7 +44,8 @@ export default class App extends Component {
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+          { (this.state.loading) ? <p>Loading..</p> : <GifList data={this.state.gifs} /> }
+          
         </div>
       </div>
     );
